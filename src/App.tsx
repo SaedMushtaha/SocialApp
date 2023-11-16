c82dd15f25731c8c4d1c8035a7bfca7c594e4390
saed-mushtaha-deltatre <125879202+saed-mushtaha-deltatre@users.noreply.github.com>
2023-11-16 09:45:37 +0300

    files and folders structured

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { Routes, Route } from "react-router-dom"
+import './globals.css';
+import AuthLayout from "./_auth/AuthLayout";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import Home from "./_root/pages/Home";
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <main className="flex h-screen">
+
+      <Routes>
+        {/* Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
